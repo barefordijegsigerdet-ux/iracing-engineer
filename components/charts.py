00391642570328542a3a9235bc5255ab8d240de8
barefordijegsigerdet-ux <barefordijegsigerdet@gmail.com>
2026-05-03 00:48:19 +0200

    Update charts.py

--- components/charts.py
+++ components/charts.py
@@ -2,25 +2,35 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 def create_main_telemetry(u_df, r_df):
-    fig = make_subplots(rows=4, cols=1, shared_xaxes=True, vertical_spacing=0.03,
-                        row_heights=[0.4, 0.2, 0.2, 0.2])
+    # Vi udvider til 5 rækker nu
+    fig = make_subplots(rows=5, cols=1, shared_xaxes=True, vertical_spacing=0.03,
+                        row_heights=[0.35, 0.15, 0.15, 0.15, 0.20])
 
-    # Speed
-    fig.add_trace(go.Scatter(x=r_df['distance'], y=r_df['speed'], name="Reference", line=dict(color='red', width=1)), row=1, col=1)
-    fig.add_trace(go.Scatter(x=u_df['distance'], y=u_df['speed'], name="Dig", line=dict(color='royalblue', width=2)), row=1, col=1)
+    # 1. Speed (km/h)
+    fig.add_trace(go.Scatter(x=r_df['distance'], y=r_df['speed'], name="Ref Speed", line=dict(color='red', width=1)), row=1, col=1)
+    fig.add_trace(go.Scatter(x=u_df['distance'], y=u_df['speed'], name="Your Speed", line=dict(color='royalblue', width=2)), row=1, col=1)
 
-    # Delta
-    fig.add_trace(go.Scatter(x=u_df['distance'], y=u_df['delta'], name="Delta", fill='tozeroy', line=dict(color='lightgrey')), row=2, col=1)
+    # 2. Gear (NY!) - Vi bruger 'hv' (hold vertical) for at få de firkantede skift
+    fig.add_trace(go.Scatter(x=r_df['distance'], y=r_df['gear'], name="Ref Gear", line=dict(color='red', width=1, dash='dot'), line_shape='hv'), row=2, col=1)
+    fig.add_trace(go.Scatter(x=u_df['distance'], y=u_df['gear'], name="Your Gear", line=dict(color='royalblue', width=2), line_shape='hv'), row=2, col=1)
 
-    # Throttle
-    fig.add_trace(go.Scatter(x=u_df['distance'], y=u_df['throttle']*100, name="Throttle", line=dict(color='#00ff00')), row=3, col=1)
+    # 3. Delta (s)
+    fig.add_trace(go.Scatter(x=u_df['distance'], y=u_df['delta'], name="Delta", fill='tozeroy', line=dict(color='lightgrey')), row=3, col=1)
 
-    # Brake
-    fig.add_trace(go.Scatter(x=u_df['distance'], y=u_df['brake']*100, name="Brake", line=dict(color='#ff0000')), row=4, col=1)
+    # 4. Throttle (%)
+    fig.add_trace(go.Scatter(x=u_df['distance'], y=u_df['throttle']*100, name="Throttle", line=dict(color='#00ff00')), row=4, col=1)
 
-    fig.update_layout(height=800, template="plotly_dark", hovermode="x unified", showlegend=False)
+    # 5. Brake (%)
+    fig.add_trace(go.Scatter(x=u_df['distance'], y=u_df['brake']*100, name="Brake", line=dict(color='#ff0000')), row=5, col=1)
+
+    fig.update_layout(height=900, template="plotly_dark", hovermode="x unified", showlegend=False)
+    
+    # Navngiv y-akserne så det er nemt at læse (ligesom i image_7b8d5c.png)
+    fig.update_yaxes(title_text="Speed", row=1, col=1)
+    fig.update_yaxes(title_text="Gear", row=2, col=1)
+    fig.update_yaxes(title_text="Delta", row=3, col=1)
+    
     return fig
-
 def create_track_map(u_df, r_df, hover_dist=0):
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=r_df['lon'], y=r_df['lat'], mode='lines', line=dict(color='#333', width=10), hoverinfo='skip'))
